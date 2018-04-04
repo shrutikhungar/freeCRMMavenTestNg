911ea8b4d18ba7a885e07e6fcb6beb253a5e0328
shruti <shrutikhungar@gmail.com>
2018-04-04 16:38:35 +0530

    added @After method to close driver

--- src/test/java/com/crm/qa/testCases/NewContactPageTest.java
+++ src/test/java/com/crm/qa/testCases/NewContactPageTest.java
@@ -2,6 +2,7 @@ package com.crm.qa.testCases;
 
 import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -48,4 +49,10 @@ public class NewContactPageTest extends TestBase{
 		homePageObj.clickNewContactLink();
 		newContactPageObj.fillNewContactDetails(title,fName,lName);
 	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	
 }
